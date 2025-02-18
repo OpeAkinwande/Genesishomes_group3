1398e37d62ac0e6f334e64415b58de2d2dca8a86
opeakinwande <opeoluwaakinwande@gmail.com>
2025-02-18 21:56:42 +0100

    DONE

--- main.js
+++ main.js
@@ -17,3 +17,10 @@ document.querySelectorAll('.add-to-cart').forEach(button =>{
     });
 
 });
+
+const menuIcon = document.querySelector('.menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('active'); // Toggle the 'active' class
+});
