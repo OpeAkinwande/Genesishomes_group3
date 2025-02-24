c02d4d46f0783c468e2a99034eb9a35eef4d0ee3
Opeoluwa Akinwande <opeoluwaakinwande@gmail.com>
2025-02-24 10:43:06 +0100

    done

--- main.js
+++ main.js
@@ -21,6 +21,18 @@ document.querySelectorAll('.add-to-cart').forEach(button =>{
 
 });
 
+const bookclick = document.querySelector('.book-click')
+const bpopup = document.querySelector('.b-popup')
+const closepopup = document.querySelector('.close-popup')
+
+bookclick.addEventListener('click', () => {
+    bpopup.classList.add('active');
+})
+
+closepopup.addEventListener('click', () => {
+    bpopup.classList.remove('active');
+})
+
 const menuIcon = document.querySelector('.menu-icon');
 const navLinks = document.querySelector('.nav-links');
 
